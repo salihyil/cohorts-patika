6c4eea43c338b5c7e395f91dda96154a21daca6c
salih <salih_yil@live.com>
2024-09-30 20:56:59 +0300

    added Pratik - If / Else

---
+++ 1-week/Pratik-if-Else/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_if_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValidInput = false;
+            int numberToCompare = 10;
+
+            while (!isValidInput)
+            {
+                Console.Write("Lütfen bir tamsayı giriniz: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int number))
+                {
+                    isValidInput = true;
+
+                    // 10 ile karşılaştırma
+                    if (number == numberToCompare)
+                    {
+                        Console.WriteLine("Girilen sayı 10'a eşittir.");
+                    }
+                    else if (number < numberToCompare)
+                    {
+                        Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+                    }
+
+                    // Çift mi tek mi kontrolü
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("Girilen sayı çifttir.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Girilen sayı tektir.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli bir tamsayı girmediniz. Lütfen tekrar deneyin.");
+                }
+            }
+
+            Console.ReadLine(); // Konsol penceresinin kapanmaması için
+        }
+    }
+}

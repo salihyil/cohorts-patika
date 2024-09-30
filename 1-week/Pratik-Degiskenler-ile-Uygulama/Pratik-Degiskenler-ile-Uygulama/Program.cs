035e0f5b196542d39d6942445914274ccf8b4a52
salih <salih_yil@live.com>
2024-09-30 15:37:58 +0300

    added Pratik - Değişkenler ile Uygulama

---
+++ 1-week/Pratik-Degiskenler-ile-Uygulama/Pratik-Degiskenler-ile-Uygulama/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Degiskenler_ile_Uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lutfen asagidaki bilgileri giriniz:");
+            long idNumber;
+
+
+            while (true)
+            {
+                Console.Write("T.C. Kimlik Numarasi (11 haneli sayi): ");
+                string input = Console.ReadLine();
+
+                if (long.TryParse(input, out idNumber) && input.Length == 11)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hata: TC kimlik numarasi 11 haneli bir sayi olmalidir. Lutfen tekrar deneyin.");
+                }
+            }
+
+            Console.Write("Adi: ");
+            string name = Console.ReadLine();
+            Console.Write("Soyadi: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Telefon Numarasi: ");
+            string phoneNumber = Console.ReadLine(); // Telefon numarası için string kullanıldı, çünkü telefon numaraları sayısal işlemler gerektirmediği için metin olarak saklanmalıdır.
+            
+            Console.Write("Yas: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("İlk aldigi urunun fiyati: ");
+            decimal firstProductPrice = decimal.Parse(Console.ReadLine()); // Ürün fiyatları için decimal kullanıldı, çünkü fiyatlar ondalıklı sayılar olabilir.
+            Console.Write("İkinci aldigi urunun fiyati: ");
+            decimal secondProductPrice = decimal.Parse(Console.ReadLine()); // Ürün fiyatları için decimal kullanıldı, çünkü fiyatlar ondalıklı sayılar olabilir.
+
+            int discountPercentage = 10;
+            decimal totalPrice = firstProductPrice + secondProductPrice;
+            decimal patikaPoint = (firstProductPrice + secondProductPrice) * 10 / 100;
+
+            Console.WriteLine("--------------------");
+            Console.WriteLine($"{idNumber} Tc numarali {name} {lastName} isimli kisi icin kayit olusturulmustur.");
+            Console.WriteLine($"{phoneNumber} numarasina bildirim mesaji gonderilmistir.");
+            Console.WriteLine($"{totalPrice} toplam harcama karsiligi kazanilan {discountPercentage}% patika puan miktarı -> {patikaPoint} TL'dir.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 1-week/Pratik-Degiskenler-ile-Uygulama/Pratik-Degiskenler-ile-Uygulama/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pratik-Degiskenler-ile-Uygulama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pratik-Degiskenler-ile-Uygulama")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72ab1d16-1b93-4e74-96f3-1b8acd9335df")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

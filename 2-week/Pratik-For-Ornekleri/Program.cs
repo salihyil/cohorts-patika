2916d8355058b53aafd6deccf1213c459293ac64
salih <salih_yil@live.com>
2024-10-02 17:35:25 +0300

    added Pratik - For Örnekleri

---
+++ 2-week/Pratik-For-Ornekleri/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_For_Ornekleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Bu pratik uygulama 5 aşamadan oluşuyor. Aşağıdaki alıştırmaları for kullanarak yapınız.
+            1 -> Konsol ekranına 10 kere "Kendime inanıyorum, ben bu yazılım işini hallederim!" yazdırınız
+            2 -> 1 ile 20 arasındaki sayıları konsol ekranına yazdırınız.
+            3 -> 1 ile 20 arasındaki çift sayıları konsol ekranına yazdırınız.
+            4 -> 50 ile 150 arasındaki sayıların toplamını ekrana yazdırınız.
+            5 -> 1 ile 120 arasındaki tek ve çift sayıların toplamlarını ayrı ayrı ekrana yazdırınız. 
+            */
+
+            #region 1.aşama
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{i}- Kendime inanıyorum, ben bu yazılım işini hallederim!");
+            }
+            #endregion
+            Console.WriteLine("\n-------------------------------------------------------------\n");
+
+            #region 2. aşama
+            for (int i = 1; i < 21; i++)
+            {
+                Console.Write(i == 20 ? $"{i}" : $"{i}, ");
+            }
+            #endregion
+            Console.WriteLine("\n\n-------------------------------------------------------------\n");
+
+            #region 3.aşama
+            for (int i = 1; i < 21; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i == 20 ? $"{i}" : $"{i}, ");
+                }
+            }
+            #endregion
+            Console.WriteLine("\n\n-------------------------------------------------------------\n");
+
+            #region 4.aşama
+            int total = 0;
+            for (int i = 50; i <= 150; i++)
+            {
+                total += i;
+            }
+            Console.Write("50 ile 150 arasındaki sayıların toplamı : " + total);
+            #endregion
+            Console.WriteLine("\n\n-------------------------------------------------------------\n");
+
+            #region 5. aşama
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= 120; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum += i;
+                }
+                else
+                {
+                    oddSum += i;
+                }
+            }
+            Console.WriteLine("Tek sayıların toplamı: " + oddSum);
+            Console.WriteLine("Çift sayıların toplamı: " + evenSum);
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 2-week/Pratik-For-Ornekleri/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pratik-For-Ornekleri")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pratik-For-Ornekleri")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1dc47f0-7373-4b2d-a09b-52cbcfd86a66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

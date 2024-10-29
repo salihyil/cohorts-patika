f01bcfdfd4432feb39d90f4b8ad496ea9efcd473
salih <salih_yil@live.com>
2024-10-29 21:34:55 +0300

    added Pratik - Try-Catch

---
+++ 5-week/Pratik-Try-Catch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pratik_Try_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayının karesi: " + sayi * sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 5-week/Pratik-Try-Catch/README.md
@@ -0,0 +1,7 @@
+# Pratik - Try-Catch
+
+Bu pratikte basit bir try-catch uygulaması yapalım.
+
+Kullanıcıdan bir sayı girmesini isteyen bir program yazın. Kullanıcı geçerli bir sayı girdiğinde bu sayının karesini ekrana yazdırınız.
+
+Kullanıcı geçersiz bir giriş yaparsa (sayı yerine harf veya sembol girmesi gibi), "Geçersiz giriş! Lütfen bir sayı giriniz." şeklinde bir hata mesajı gösteriniz.

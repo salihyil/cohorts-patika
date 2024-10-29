69d1e470eed80f3c33d9c0ebc2f48348eb9ee0d1
salih <salih_yil@live.com>
2024-10-29 15:48:20 +0300

    added  Pratik - Lists 2

---
+++ 5-week/Pratik-Lists-2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Pratik_Lists_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffeeList = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($" Kahve {i + 1} : ");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                coffeeList.Add(Console.ReadLine());
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("\nGirilen kahve isimleri : ");
+
+            foreach (var coffee in coffeeList)
+            {
+                Console.WriteLine(coffee);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5-week/Pratik-Lists-2/README.md
@@ -0,0 +1,13 @@
+# Pratik - Lists 2
+
+Bir kahve içsek bize iyi gelecek!
+
+kullanıcının konsoldan gireceği 5 kahve ismiyle bir liste oluşturup, bu listeyi foreach döngüsüyle yazdıralım.
+
+Örnek veri giriş ekranı :
+
+![image](ornek-veri-giris-ekrani.png)
+
+Örnek Program Çıktısı:
+
+![image](ornek-program-cikti.png)

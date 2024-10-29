6bed4479b60b844061d5b90e5915a22b12c367d5
salih <salih_yil@live.com>
2024-10-29 15:29:26 +0300

    added Pratik - Lists 1

---
+++ 5-week/Pratik-Lists-1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Pratik_Lists_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu Pratik'te sizden ziyaretçilerin isimlerini eklediğimiz bir liste oluşturmanızı ve bu liste içerisinde for ya da foreach döngüsü ile dönerek isimlerini tek tek ekrana yazdırmanız isteniyor.
+
+            /* ** Davetliler * *
+            1 - Bülent Ersoy
+            2 - Ajda Pekkan
+            3 - Ebru Gündes
+            4 - Hadise
+            5 - Hande Yener
+            6 - Tarkan
+            7 - Funda Arar
+            8 - Demet Akalin */
+
+            Console.WriteLine("** Davetliler * *");
+            List<string> guests = ["Bülent Ersoy", "Ajda Pekkan", "Ebru Gündes", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalin"];
+
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {guests[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5-week/Pratik-Lists-1/README.md
@@ -0,0 +1,9 @@
+# Pratik - Lists 1
+
+Patika Plus gala gecesine davetlisiniz!
+
+Bu Pratik'te sizden ziyaretçilerin isimlerini eklediğimiz bir liste oluşturmanızı ve bu liste içerisinde for ya da foreach döngüsü ile dönerek isimlerini tek tek ekrana yazdırmanız isteniyor.
+
+## 📝 Ödev
+
+![Pratik - Lists 1](Pratik-Lists-1.png)
